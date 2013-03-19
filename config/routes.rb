607cca7caf1d8666314b010aaf4e996d070cdfe1
Jeremy Belcher <charlesthefourth@gmail.com>
2013-03-19 00:59:42 -0400

    errors! not sure why

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Omrails::Application.routes.draw do
-  get 'about' => 'pages#about'
+  devise_for :users
 
-  root :to => 'pages#home'
+ 
+    get 'about' => 'pages#about'
+    root :to => 'pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
