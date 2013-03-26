ad1370c3aa0e06a498b7ee38217360df7bc5b2b4
Jeremy Belcher <charlesthefourth@gmail.com>
2013-03-25 22:01:40 -0400

    more fb login attempts

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,3 +11,16 @@ class User < ActiveRecord::Base
 
   has_many :pins
 end
+
+def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+  user = User.where(:provider => auth.provider, :uid => auth.uid).first
+  unless user
+    user = User.create(name:auth.extra.raw_info.name,
+                         provider:auth.provider,
+                         uid:auth.uid,
+                         email:auth.info.email,
+                         password:Devise.friendly_token[0,20]
+                         )
+  end
+  user
+end
